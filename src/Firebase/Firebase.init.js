19625b5835225dfbeda0ad8d05405d05e7dfa277
ashik255 <ariful15-11556@diu.edu.bd>
2021-10-13 03:43:02 +0600

    first commit

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9_l4WXQAd5LPapGMxDuLDi0tlgYcOVNk",
+    authDomain: "ema-john-ca28d.firebaseapp.com",
+    projectId: "ema-john-ca28d",
+    storageBucket: "ema-john-ca28d.appspot.com",
+    messagingSenderId: "469341902366",
+    appId: "1:469341902366:web:e2002f10d914ed9732400f"
+  };
+
+  export default firebaseConfig;
